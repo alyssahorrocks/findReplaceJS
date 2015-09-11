259e7ed8261b184a2292e3d514581cfe5c4d9f5b
Alyssa Horrocks <am.rocks@outlook.com>
2015-09-11 13:07:30 -0400

    Replace a word regardless of punctuation at the beginning or end.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,20 @@
 var findReplace = function(phrase, wordFind, wordReplace) {
   var phraseArray = phrase.split(" ");
   var replacementArray = []
-  
+
   for(var i = 0; i < phraseArray.length; i+=1) {
-    if (phraseArray[i] === wordFind) {
-      replacementArray.push(wordReplace);
+    var wordOn = phraseArray[i].split("");
+
+    if (/[,.?\-!''""]/.test(wordOn[wordOn.length - 1])) {
+      var punc = wordOn.pop();
+    } else if (/[,.?\-!''""]/.test(wordOn[0])) {
+      var punc = wordOn.shift();
+    } else {
+      var punc = "";
+    };
+
+    if (wordOn.join("") === wordFind) {
+      replacementArray.push(wordReplace + punc);
     } else {
       replacementArray.push(phraseArray[i]);
     };
--- spec/specs.js
+++ spec/specs.js
@@ -6,4 +6,8 @@ describe('findReplace', function() {
   it('replace multiple instances of a word with another', function() {
     expect(findReplace('the green ridge mountains are green', 'green', 'blue')).to.equal('the blue ridge mountains are blue');
   });
+
+  it('replace words in a phrase regardless of beginning or ending punctuation', function() {
+    expect(findReplace('the near sand, the dear sand, whatever fate', 'sand', 'land')).to.equal('the near land, the dear land, whatever fate');
+  });
 });
